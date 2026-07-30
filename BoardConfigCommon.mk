d40bf5569a6084930378d4e8c95eb67647b5b3dc
pix106 <sbordenave@gmail.com>
2026-07-30 10:26:59 +0200

    Revert "sdm660-common: Enable RAW10_BUFFER_FIX with soong"
    This reverts commit 5bd23a3323a575a9b744c0821ba7b3afdeff7450.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,7 +70,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
 # Display
-SOONG_CONFIG_qtidisplay_target_needs_raw10_buffer_fix := true
+TARGET_NEEDS_RAW10_BUFFER_FIX := true
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
