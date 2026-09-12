10998a4faf3ae46ad02b4a415fec7e7aac08070a
Maaaul <86137265+Maaaul@users.noreply.github.com>
2026-09-12 23:39:21 +0000

    sdm660-common: health: Add charging control configurations
    Signed-off-by: Maaaul <86137265+Maaaul@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -247,6 +247,11 @@ endif
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
